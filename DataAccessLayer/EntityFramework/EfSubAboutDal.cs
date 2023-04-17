e5b393fb523fe8f2e8d1ca45aa6b415dd9baad6a
Onur Keçe <onurkecee@gmail.com>
2023-04-18 00:39:04 +0300

    Bazı veriler sql'den çekildi. anasayfa daha düzenli hale geldi. ViewComponent yapısına devam edildi

--- DataAccessLayer/EntityFramework/EfSubAboutDal.cs
+++ DataAccessLayer/EntityFramework/EfSubAboutDal.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-   public class EfSubAboutDal : GenericRepository<SubAbout>, ISubAboutDal
+    public class EfSubAboutDal : GenericRepository<SubAbout>, ISubAboutDal
     {
     }
 }
---
+++ TraversalCoreProje/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _SubAbout : ViewComponent
+    {
+        SubAboutManager subAboutManager = new SubAboutManager(new EfSubAboutDal());
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = subAboutManager.TGetList();
+            return await Task.FromResult((IViewComponentResult)View(values));
+        }
+    }
+}
